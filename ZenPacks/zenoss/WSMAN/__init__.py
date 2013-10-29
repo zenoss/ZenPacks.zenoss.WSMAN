ac1c579504d7581f8f63a0a7197068bcb6a6576c
Eric Edgar <eedgar@zenoss.com>
2013-10-29 07:37:20 -0500

    Initial commit

---
+++ ZenPacks/zenoss/WSMAN/__init__.py
@@ -0,0 +1,30 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2012, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+import logging
+LOG = logging.getLogger('zen.WSMAN')
+
+from Products.ZenModel.ZenPack import ZenPackBase
+from Products.ZenRelations.zPropertyCategory import setzPropertyCategory
+
+
+# Categorize our zProperties.
+setzPropertyCategory('zWSMANPort', 'WSMAN')
+setzPropertyCategory('zWSMANUsername', 'WSMAN')
+setzPropertyCategory('zWSMANPassword', 'WSMAN')
+
+
+class ZenPack(ZenPackBase):
+    """WSMAN ZenPack."""
+
+    packZProperties = [
+        ('zWSMANPort', '443', 'integer'),
+        ('zWSMANUsername', '', 'string'),
+        ('zWSMANPassword', '', 'password'),
+        ]
