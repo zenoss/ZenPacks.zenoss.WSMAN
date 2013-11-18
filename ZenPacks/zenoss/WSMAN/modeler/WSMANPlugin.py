0596e4ea9afcccc27d4023a681f7ff4ba2b4792e
Eric Edgar <eedgar@zenoss.con>
2013-11-19 03:39:56 +0530

    Example modeller plugin for idrac...

--- ZenPacks/zenoss/WSMAN/modeler/WSMANPlugin.py
+++ ZenPacks/zenoss/WSMAN/modeler/WSMANPlugin.py
@@ -94,69 +94,9 @@ class WSMANPlugin(PythonPlugin):
         
         try:
             results = yield client.do_enumerate( map(self.create_enum_info, self.wsman_queries, self.wsman_queries.values()))
-            import pdb;pdb.set_trace()
 
         except txwsman.util.RequestError as e:
             log.error(e[0])
             raise
 
-
         defer.returnValue(results)
-
-        '''
-        if not self.wql_queries:
-            pass
-    
-        try:
-            pass
-        except Exception as e:
-            log.error(e[0])
-            raise
-
-            elif wbemclass == 'ei':
-                wbemClass = EnumerateInstances(
-                    userCreds, namespace=namespace, classname=classname)
-
-            elif wbemclass == 'ein':
-                wbemClass = EnumerateInstanceNames(
-                    userCreds, namespace=namespace, classname=classname)
-
-            else:
-                log.warn('Incorrect class call %s', wbemclass)
-                wbemClass = EnumerateClasses(userCreds,
-                                             namespace=namespace)
-
-            deferreds.append(wbemClass.deferred)
-
-            if device.zWSMANUseSSL == True:
-                reactor.connectSSL(
-                    host=device.manageIp,
-                    port=int(device.zWSMANPort),
-                    factory=wbemClass,
-                    contextFactory=ssl.ClientContextFactory())
-            else:
-                reactor.connectTCP(
-                    host=device.manageIp,
-                    port=int(device.zWSMANPort),
-                    factory=wbemClass)
-
-        # Execute the deferreds and return the results to the callback.
-        d = DeferredList(deferreds, consumeErrors=True)
-        d.addCallback(self.check_results, device, log)
-
-        return d
-        '''
-
-    def check_results(self, results, device, log):
-        """Check results for errors."""
-
-        # If all results are failures we have a problem to report.
-        if len(results) and True not in set(x[0] for x in results):
-            log.error('%s WSMAN: %s', device.id, result_errmsg(results[0][1]))
-
-            #This will allow for an event to be created by the device class.
-            results = "ERROR", result_errmsg(results[0][1])
-
-            return results
-
-        return results
---
+++ ZenPacks/zenoss/WSMAN/modeler/plugins/wsman/ExampleModeler.py
@@ -0,0 +1,30 @@
+
+from ZenPacks.zenoss.WSMAN.modeler.WSMANPlugin import WSMANPlugin
+
+class ExampleModeler(WSMANPlugin):
+     compname = 'hw'
+     relname = 'cpus'
+     modname = 'Products.Zenmodel.Cpu.Cpu'
+
+     #wsman_queries = {'DCIM_ComputerSystem': '',
+     #                 'CIM_Component': '' }
+     wsman_queries = {'DCIM_VirtualDiskView': '' ,
+                      'DCIM_CPUView': '' ,
+                      'DCIM_MemoryView': '' ,
+                      'DCIM_FanView': '' ,
+                      'DCIM_iDRACCardView': '' ,
+                      'DCIM_PCIDeviceView': '' ,
+                      'DCIM_VideoView': '' ,
+                      'DCIM_ControllerView': '' ,
+                      'DCIM_BIOSEnumeration': '' ,
+                      'DCIM_SystemView': '' ,
+                      'DCIM_VFlashView': '' ,
+                      'DCIM_NICView': '' }
+
+     def process(self, device, results, log):
+         log.info("Modeler %s Processing data for device %s",
+             self.name(), device.id)
+         log.info("Example accessing a results metric: %s" % results[0][0].BusNumber)
+         import pdb;pdb.set_trace()
+
+
