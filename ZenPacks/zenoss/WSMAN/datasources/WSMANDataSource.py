afb6a43289f8b1a75e41fb2e0e7d9f76210b1ae6
Oleksandr Brazhnyk <itshane@gmail.com>
2018-10-24 18:12:57 +0300

    Extend the existing data handler to map the incoming results with the existing datasources basing on the approximate similarity.
    Fixes ZPS-4590

--- ZenPacks/zenoss/WSMAN/datasources/WSMANDataSource.py
+++ ZenPacks/zenoss/WSMAN/datasources/WSMANDataSource.py
@@ -224,20 +224,30 @@ class WSMANDataSourcePlugin(PythonDataSourcePlugin):
         result_component_key = ds0.params['result_component_key']
 
         for result in results:
-            if result_component_key:
-                datasource = datasources.get(result[result_component_key])
+            component_key = getattr(result, result_component_key)
+
+            if component_key:
+                datasource = datasources.get(component_key)
 
                 if not datasource:
-                    continue
+                    compatible_datasources = [
+                        datasources[k]
+                        for k in datasources.iterkeys()
+                        if k in component_key]
+
+                    if compatible_datasources:
+                        datasource = compatible_datasources[0]
+                    else:
+                        continue
             else:
                 datasource = ds0
 
-            if result_component_key and hasattr(result, result_component_key):
-                result_component_value = datasource.params.get(
-                    'result_component_value')
+            result_component_value = datasource.params.get(
+                'result_component_value')
 
-                if result_component_value != result[result_component_key]:
-                    continue
+            if result_component_value not in component_key:
+                continue
+
             if datasource.component:
                 component_id = prepId(datasource.component)
             else:
@@ -247,14 +257,11 @@ class WSMANDataSourcePlugin(PythonDataSourcePlugin):
             result_timestamp_key = datasource.params.get(
                 'result_timestamp_key')
 
-            timestamp = None
+            timestamp = 'N'
 
             if result_timestamp_key and result_timestamp_key in result:
                 cim_date = result[result_timestamp_key]
                 timestamp = calendar.timegm(cim_date.datetime.utctimetuple())
-
-            if not timestamp:
-                timestamp = 'N'
 
             for datapoint in datasource.points:
                 if hasattr(result, datapoint.id):
