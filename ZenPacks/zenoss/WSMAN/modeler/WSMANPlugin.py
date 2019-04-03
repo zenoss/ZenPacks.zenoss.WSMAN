701164fa4ec6161871e8b39544bdad78db9b10c7
Volodymyr Serheiev <vserh@softserveinc.com>
2019-04-03 11:48:24 -0500

    ZPS-4759: added events for SSL disables case - both in modeling and monitoring
    (cherry picked from commit 79be9722094f2925f536aadffa0710e9e8482043)
    Signed-off-by: Roman Bakaleyko <roman.bakaleyko@gmail.com>

--- ZenPacks/zenoss/WSMAN/modeler/WSMANPlugin.py
+++ ZenPacks/zenoss/WSMAN/modeler/WSMANPlugin.py
@@ -94,6 +94,22 @@ class WSMANPlugin(PythonPlugin):
 
         if not device.zWSMANUseSSL:
             log.warning("SSL not enabled for %s", device.id)
+            self._eventService.sendEvent({
+                'device': device.id,
+                'eventKey': "{}|{}".format(device.id, 'wsmanCollectSsl'),
+                'eventClassKey': 'wsmanCollect',
+                'severity': ZenEventClasses.Warning,
+                'summary': 'WSMAN: SSL not enabled',
+                'message': 'SSL not enabled for {}'.format(device.id),
+            })
+        else
+            self._eventService.sendEvent({
+                'device': device.id,
+                'eventKey': "{}|{}".format(device.id, 'wsmanCollectSsl'),
+                'eventClassKey': 'wsmanCollect',
+                'severity': ZenEventClasses.Clear,
+                'summary': 'WSMAN: SSL enabled',
+            })
 
         conn_info = self.conn_info(device)
         client = self.client(conn_info)
